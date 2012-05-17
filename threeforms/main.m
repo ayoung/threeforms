84ae0856fbe4b241a6ea3399819c6ae6e8007580
Andrew Young <andrewdyoung@gmail.com>
2012-05-16 22:27:37 -0700

    Initial Commit

---
+++ threeforms/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  threeforms
+//
+//  Created by Andrew Young on 5/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TFAppDelegate class]));
+    }
+}
